d92f57ad1b6c0bc958213091c93618c6304c22ff
pawantech12 <pawankumavat042@gmail.com>
2025-06-11 10:38:14 +0530

    fixed prerender issue

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,6 +1,7 @@
 import { DashboardNav } from "@/components/dashboard-nav";
 import { DashboardHeader } from "@/components/dashboard-header";
 import { Toaster } from "sonner";
+import { Suspense } from "react";
 
 export default function DashboardLayout({ children }) {
   return (
@@ -8,7 +9,9 @@ export default function DashboardLayout({ children }) {
       <DashboardHeader />
       <div className="flex flex-1">
         <DashboardNav className="hidden md:flex" />
-        <main className="flex-1 p-5">{children}</main>
+        <main className="flex-1 p-5">
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        </main>
       </div>
       <Toaster />
     </div>
